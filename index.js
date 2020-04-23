09ad3790817c7d5af7ed85b2b1cf3497807177d6
thedannysoto <87dsoto@gmail.com>
2020-04-23 10:42:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+
+
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    }; 
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+  
+    fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        console.log(object);
+        let element = document.createElement('p');
+        element.innerHTML = `${object.id}`;
+        document.querySelector('body').appendChild(element);
+    })
+    .catch(function(error) {
+        console.log(error.message);
+        alert("Bad things! Ragnarők!");
+        let element = document.createElement('p');
+        element.innerHTML = error.message;
+        document.querySelector('body').appendChild(element);
+    });
+}
