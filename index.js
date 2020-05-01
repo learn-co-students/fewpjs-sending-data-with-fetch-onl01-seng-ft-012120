600628f46eb8e1a1a5f3c7df8117d7698aa265ea
andrew041994 <andrew041994@gmail.com>
2020-05-01 01:32:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+
+function submitData(n, e) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: n,
+            email: e 
+        }) 
+
+    })
+    .then(function(response) {
+      
+       return response.json();
+    }) 
+
+    .then(function(object) {
+        let id = object.id
+        updateBody(id)
+      })
+      .catch(function(error) {
+          alert("oh no!");
+          errors(error.message);
+      })
+   
+}
+
+function updateBody(id) {   
+   document.body.innerHTML = id 
+}
+
+function errors(message) {
+    document.body.innerHTML = message
+}
